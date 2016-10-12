821761da4322b4af63d19448d0148e8b2af847c1
Вадим Новосельцев <acedened@MacBook-Pro-Vadim.local>
2016-10-12 22:54:27 +0300

    Add Alamofire, setup Cocoapods

---
+++ TheChan/Facade.swift
@@ -0,0 +1,16 @@
+//
+//  Facade.swift
+//  TheChan
+//
+//  Created by Вадим Новосельцев on 12.10.16.
+//  Copyright © 2016 ACEDENED Software. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class Facade {
+    static func loadBoards() -> [BoardsGroup]? {
+        return nil
+    }
+}
