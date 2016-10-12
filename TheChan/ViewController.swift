41b65ab620572ea6d18727168b03d025e341fc68
Вадим Новосельцев <acedened@MacBook-Pro-Vadim.local>
2016-10-12 22:14:19 +0300

    Initial Commit

---
+++ TheChan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TheChan
+//
+//  Created by Вадим Новосельцев on 12.10.16.
+//  Copyright © 2016 ACEDENED Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
