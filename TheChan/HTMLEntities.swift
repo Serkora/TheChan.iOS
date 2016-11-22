117060752a04dc3fbabd0343c13c2dc08656e11b
Vadim Novoseltsev <acedened@outlook.com>
2016-11-22 18:06:14 +0300

    Speed up subject parsing

--- TheChan/HTMLEntities.swift
+++ TheChan/HTMLEntities.swift
@@ -7,26 +7,13 @@
 //
 
 import UIKit
+import Kanna
 
 extension String {
     init(htmlEncodedString: String) {
         self.init()
-        guard let encodedData = htmlEncodedString.data(using: .utf8) else {
-            self = htmlEncodedString
-            return
-        }
-        
-        let attributedOptions: [String : Any] = [
-            NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
-            NSCharacterEncodingDocumentAttribute: String.Encoding.utf8.rawValue
-        ]
-        
-        do {
-            let attributedString = try NSAttributedString(data: encodedData, options: attributedOptions, documentAttributes: nil)
-            self = attributedString.string
-        } catch {
-            print("Error: \(error)")
-            self = htmlEncodedString
-        }
+        let document = HTML(html: htmlEncodedString, encoding: .utf8)
+        let text = document?.body?.text ?? htmlEncodedString
+        self = text
     }
 }
