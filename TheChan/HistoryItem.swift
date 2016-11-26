075c56e18389c7b5340c891cdf8eb1d6fd47fbab
Vadim Novoseltsev <acedened@outlook.com>
2016-11-26 17:17:37 +0300

    HistoryItem class

---
+++ TheChan/HistoryItem.swift
@@ -0,0 +1,17 @@
+//
+//  HistoryThreadItem.swift
+//  TheChan
+//
+//  Created by Вадим Новосельцев on 26.11.16.
+//  Copyright © 2016 ACEDENED Software. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
+
+class HistoryItem: Object {
+    dynamic var board = ""
+    dynamic var number = 0
+    dynamic var name = ""
+    dynamic var lastVisit = Date()
+}
